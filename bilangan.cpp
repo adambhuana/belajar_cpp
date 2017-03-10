ab5c7b172ddffbf15b277d57af810fc6f03566f0
adambhuana <adam.bhuana@gmail.com>
2017-03-10 11:29:08 +0700

    Create bilangan.cpp

---
+++ bilangan.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+ using namespace std;
+
+ int main() {
+ int bilawal;
+ bilawal=0;
+ cout << "Nilai awal Bilangan adalah 0 \n"<<endl; 
+ cout << "Masukkan Bilangan:" << endl;
+ cin>>bilawal;
+ if(bilawal>0)
+ {
+	 cout << "Bilangan Input Lebih Besar dari Nol \n";
+ }
+ if(bilawal<0)
+ {
+	 cout <<"Bilangan Input Lebih kecil dari Nol \n";
+ }
+ if(bilawal==0)
+ {
+	 cout <<"Bilangan Input sama dengan Nol \n";
+ }
+
+ }
