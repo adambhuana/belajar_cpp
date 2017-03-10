6ac93c926d793d373cd5bfb2e5f19622a16b26c1
adambhuana <adam.bhuana@gmail.com>
2017-03-10 11:37:54 +0700

    Create lat_array.cpp

---
+++ lat_array.cpp
@@ -0,0 +1,16 @@
+#include <iostream.h>
+#include <conio.h>
+
+int main()
+{
+	int y[6];
+	int n,r;
+	for(n=0;n<6;n++)
+	{
+		cout<<"Masukkan Nilai Indeks"<<n<<endl;
+		cin>>y[n];
+	}
+	//cout<<""<<r;
+	//getch();
+	return 0;
+}
